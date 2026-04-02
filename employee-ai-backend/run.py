a5052085cc387ded186b1c83a9a68e0d6794d24a
boshini31 <boshinitp@gmail.com>
2026-04-02 07:22:22 +0530

    Hosted

--- employee-ai-backend/run.py
+++ employee-ai-backend/run.py
@@ -1,4 +1,8 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    host = "0.0.0.0" if os.environ.get("RENDER") else "127.0.0.1"
+    
+    uvicorn.run("app.main:app", host=host, port=port, reload=True)
